07d4c30e324118b9e6561d693b0017d9881c8449
jweken <jweken@kpnmail.nl>
2021-08-21 10:51:53 +0200

    update greet and main

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import greet
 
 greet
+element = greet.Greeting()
 print(greet.__doc__)
-print(greet.Greeting())
+print(element)
+print(element[0])
+print(element[2])
+print(type(greet))
+print(type(greet.Greeting()))
+print(type(element[2]))
